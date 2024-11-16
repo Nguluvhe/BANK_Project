60008cdf9f5ccc9b35619dd7e4db664b3ee8e002
Nguluvhe <38410147+Nguluvhe@users.noreply.github.com>
2024-11-16 19:00:23 +0200

    fo

--- Models/ViewModels/ForgotPasswordModel.cs
+++ Models/ViewModels/ForgotPasswordModel.cs
@@ -1,6 +1,11 @@
-﻿namespace UFS_BANK_FINAL.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UFS_BANK_FINAL.Models.ViewModels
 {
     public class ForgotPasswordModel
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }
